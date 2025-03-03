06df8a65536acb099a7eb2d79948ac018c0e86a5
hamza Dazia <135356014+HamzaDazIA@users.noreply.github.com>
2025-03-03 20:29:09 +0000

    Update d.c

--- d.c
+++ d.c
@@ -2,43 +2,34 @@ int	is_float(char *str)
 {
 	int	i_float;
 	int	i;
-	int	has_digit;
-
-	i_float = 0;
-	has_digit = 0;
-	i = 0;
-	// Skip leading whitespace
+	double value;
+	
+	(1) && (i_float = 0, i = 0, value = 0.0);
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	// Check for optional sign
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	// Check first character is a digit or dot
-	if (str[i] == '.')
-		i_float = 1;
-	else if (ft_isdigit(str[i]))
-		has_digit = 1;
-	else
-		return (0); // Not a digit or dot
-	// Process remaining characters for number part
-	i++;
-	while (str[i])
+	if (!ft_isdigit(str[i]) && str[i] != '.')
+		return (0);
+	while (str[i] && str[i] != ' ')
 	{
 		if (str[i] == '.')
 		{
 			if (i_float)
-				return (0); // Multiple dots
+				return (0);
 			i_float = 1;
 		}
-		else if (ft_isdigit(str[i]))
-			has_digit = 1;
+		else if (!(ft_isdigit(str[i])))
+			return (0);
 		else
-			break ; // End of number part
+			(1) && (value = value * 10 + (str[i] - '0'), check_overflow(value));
 		i++;
 	}
-	// Skip trailing whitespace
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+	while(str[i])
+	{
+		if(str[i] != ' ')
+			return (0);
 		i++;
-	// Check if all characters are processed and at least one digit is present
-	return (str[i] == '\0' && has_digit);
+	}
+	return (1);
 }
