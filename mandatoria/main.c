e4d64f244262ae68cbb215a0552f5ac0f3298df5
hamza Dazia <135356014+HamzaDazIA@users.noreply.github.com>
2025-03-04 00:28:55 +0000

    Update main.c

--- mandatoria/main.c
+++ mandatoria/main.c
@@ -6,7 +6,7 @@
 /*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 17:15:03 by hdazia            #+#    #+#             */
-/*   Updated: 2025/03/01 05:22:48 by hdazia           ###   ########.fr       */
+/*   Updated: 2025/03/03 23:04:09 by hdazia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,14 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (HEIGHT <= 0 || HEIGHT > 2500 || WIDTH <= 0 || WIDTH > 2500 || argc < 2)
+	if (HEIGHT <= 0 || HEIGHT > 2500 || WIDTH <= 0 || WIDTH > 2500)
 		return (1);
-	if (N_MAX > 2 || N_MAX < -2 || N_MIN < -2 || N_MIN > 2)
+	else if (N_MAX > 2 || N_MAX < -2 || N_MIN < -2 || N_MIN > 2)
 		return (1);
-	if (parsin(argc, argv, &data) == 0)
-	{
+	else if (argc < 2)
 		print_usage();
-		return (1);
-	}
+	else if (parsin(argc, argv, &data) == 0)
+		print_usage();
 	else if (mlx_initialization(&data) == 1)
 	{
 		write(2, "Error initializing MiniLibX\n", 28);
